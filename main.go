506de43d20ff95a066a22f12e0ba3f3d82c87c58
hendisantika <hendisantika@yahoo.co.id>
2023-01-03 20:07:46 +0700

    #3 | Add dbConn ✏️ 📗 🐳⬆

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"os"
 )
 
 // Tool struct
@@ -12,4 +15,20 @@ type Tool struct {
 	URL      string
 	Rating   int
 	Notes    string
+}
+
+func dbConn() (db *sql.DB) {
+	dbDriver := "mysql"
+	dbUser := os.Getenv("DATABASE_USERNAME")
+	dbPass := os.Getenv("DATABASE_PASSWORD")
+	dbName := os.Getenv("DATABASE_NAME")
+	dbServer := os.Getenv("DATABASE_SERVER")
+	dbPort := os.Getenv("DATABASE_PORT")
+	log.Println("Database host: " + dbServer)
+	log.Println("Database port: " + dbPort)
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp("+dbServer+":"+dbPort+")/"+dbName)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
 }
